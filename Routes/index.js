d8a5c4400d40686f4e1b7c26b72d59fbba28dd47
Ange-Chrisostone <Chrisostone.niamke07@gmail.com>
2024-11-13 18:31:00 -0500

    udapte

--- Routes/index.js
+++ Routes/index.js
@@ -14,17 +14,17 @@ import UtilisateursRoutes from './UtilisateursRoutes.js';
 
 const router = express.Router();
 
-router.use('/adresses', AdresseRoutes);
+router.use('/adresse', AdresseRoutes);
 router.use('/avis', AvisRoutes);
-router.use('/categories', CategorieRoutes);
+router.use('/categorie', CategorieRoutes);
 router.use('/commandes', CommandesRoutes);
-router.use('/coupons', CouponRoutes);
+router.use('/coupon', CouponRoutes);
 router.use('/detailscommande', DetailsCommandeRoutes);
-router.use('/imagesproduits', ImageProduitsRoutes);
+router.use('/imageproduits', ImageProduitsRoutes);
 router.use('/modedepaiement', ModeDePaiementRoutes);
 router.use('/panier', PanierRoutes);
 router.use('/produits', ProduitsRoutes);
-router.use('/roles', RoleRoutes);
+router.use('/role', RoleRoutes);
 router.use('/utilisateurs', UtilisateursRoutes);
 
 export default router;
